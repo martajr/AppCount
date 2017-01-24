09d13cc209409ce11a64d1b83bf9c4c6e94d77be
marta <marta.jimenez@alu.ulpgc.es>
2017-01-24 11:46:46 +0000

    falta layaout

--- app/src/main/java/com/example/marta/appcount/MainActivity.java
+++ app/src/main/java/com/example/marta/appcount/MainActivity.java
@@ -4,12 +4,31 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+
+    private TextView display;
+    private Button boton;
+    private Integer contador;
+
+
+    class MyButtonListener implements OnClickListener{
+        @Override
+        public void onClick(View view){
+            contador++;
+            display.setText(contador.toString());
+        }
+    }
     //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contador =0;
+        boton =(Button)findViewById(R.id.button1);
+        display=(TextView)findViewById(R.id.textView);
+
+        boton.setOnClickListener(new MyButtonListener());
     }
 
 }
